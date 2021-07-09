1d682aa48d326fbc85eb02c7e1fe4a9a36dc6871
unknown <Yohana Fransisca@PLZNB0489200200.Supporting.corp.bankmandiri.co.id>
2021-07-09 13:28:37 +0700

    fix js

--- modul-1/session-1/homework/main.js
+++ modul-1/session-1/homework/main.js
@@ -1,3 +1,13 @@
-const submitAlert = () => {
-    alert("create playlist successful.");
-  };
+const playlistForm = document.getElementById('playlist-form');
+
+playlistForm.addEventListener('submit', function(event) {
+    event.preventDefault()
+
+    alert("successfully create playlist.");
+
+    const playlistTitle = document.getElementById('playlist-title').value
+    const playlistDesc = document.getElementById('playlist-desc').value
+
+    console.log(playlistTitle)
+    console.log(playlistDesc)
+});
