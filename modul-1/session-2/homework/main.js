76bae8dbecc7a4a9be7ae65085b7142f42f2cc44
unknown <Yohana Fransisca@PLZNB0489200200.Supporting.corp.bankmandiri.co.id>
2021-07-10 22:14:39 +0700

    fix js

--- modul-1/session-2/homework/main.js
+++ modul-1/session-2/homework/main.js
@@ -1,3 +1,5 @@
+// Create Playlist
+
 const playlistForm = document.getElementById('playlist-form');
 
 playlistForm.addEventListener('submit', function(event) {
@@ -12,41 +14,28 @@ playlistForm.addEventListener('submit', function(event) {
     console.log(playlistDesc)
 });
 
-// const xhr = new XMLHttpRequest();
-// xhr.onreadystatechange = function () {
-//   if (xhr.status === 200) {
-//     if (xhr.readyState === 4) {
-//       console.log(JSON.parse(xhr.response));
-//     }
-//   } else {
-//     console.log(xhr.responseText);
-//   }
-// }
-// xhr.open('get', 'https://gist.githubusercontent.com/aryapradipta9/e6492383477803b233916e01f36d5465/raw/66942c739d66d3774303f84071696aa865a07077/single-sample.json');
-// xhr.send();
+// GET Track Info
 
-
-// fetch('https://gist.githubusercontent.com/aryapradipta9/e6492383477803b233916e01f36d5465/raw/66942c739d66d3774303f84071696aa865a07077/single-sample.json')
-//     .then(response => response.json())
-//     .then(response => console.log(response)); //agar bentuknya object
-
-const getSong = () => {
-  fetch('https://gist.githubusercontent.com/aryapradipta9/e6492383477803b233916e01f36d5465/raw/66942c739d66d3774303f84071696aa865a07077/single-sample.json')
-    .then((response) => {
-      if (!response.ok) {
-        return Promise.reject("there is an error fetching the data");
-      }
-      console.log("status ok", response);
-      return response.json();
-    })
-    .then((data) => {
-      console.log("successfully retrieve song info.", data);
-    })
+fetch('https://gist.githubusercontent.com/aryapradipta9/e6492383477803b233916e01f36d5465/raw/66942c739d66d3774303f84071696aa865a07077/single-sample.json')
+    .then(response => response.json())
+    .then(response => {
+        console.log(response);
+        const songInfo = showSongInfo(response);
+        const songCard = document.getElementById('song-card');
+        songCard.innerHTML = songInfo;
+    }) 
     .catch((err) => {
-      alert("unable to retrieve song info.", err);
+        alert("unable to retrieve song info.", err);
     });
-};
-getSong();
+
+
+function showSongInfo(response) {
+  return `<img class="song-image" src="${response.album.images[0].url}">
+          <p class="song-title">${response.name}</p>
+          <p class="singer">${response.artists[0].name}</p>
+          <p class="album">${response.album.name}</p>
+          <button class="btn" href="${response.album.external_urls.spotify}">Open in Spotify</button>`;
+}
 
 
 
