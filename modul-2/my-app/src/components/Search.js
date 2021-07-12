6b4019b8c67139bc8c85a858d10cb6dab956fa26
unknown <Yohana Fransisca@PLZNB0489200200.Supporting.corp.bankmandiri.co.id>
2021-07-12 18:46:41 +0700

    add hands-on 3

---
+++ modul-2/my-app/src/components/Search.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Search() {
+    return (
+        <div>
+            <input type="text"></input>
+            <input type="submit" value="Submit"></input>
+            <div>
+            <img src="https://media.giphy.com/media/Vh8pbGX3SGRwFDh3V0/source.gif"></img>
+            </div>
+        </div>
+    )
+}
+
+export default Search
